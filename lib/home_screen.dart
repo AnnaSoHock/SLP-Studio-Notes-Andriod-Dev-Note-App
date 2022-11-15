e77d2cdce54ec388ed8a64e65bca55b49c462c39
MadCodes9 <ceiaram@cpp.edu>
2022-11-14 20:11:01 -0800

    Fixed ListTile error

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'widgets/note_card.dart';
+import 'note_card.dart';
 
 import 'main.dart';
 
@@ -11,64 +11,48 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  //home screen actions
+class _HomeScreenState extends State<HomeScreen> {  //home screen actions
   @override
-  Widget build(BuildContext context) {
-    //entire UI
-
-
-      return MaterialApp(
-          home: Scaffold( //default UI
+  Widget build(BuildContext context) {  //entire UI
+    return Scaffold(    //default UI
       body: Center(
-      child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, //alignment
+        child: ListView(  //ListView child of Center
           children: <Widget>[
-            Row(
-              children: const <Widget>[
-                Expanded(
-                  child: ListTile(
-                    leading: GestureDetector(
-                      behavior: HitTestBehavior.translucent,
-                      onTap: () {},
-                      child: Container(
-                        width: 48,
-                        height: 48,
-                        padding: const EdgeInsets.symmetric(vertical: 4.0),
-                        alignment: Alignment.center,
-                        child: const CircleAvatar(),
-                      ),
-                    ),
-
-
-                    title: Text('These ListTiles are expanded '),
-
-                  ),
-                ),
-                Expanded(
-                  child: ListTile(
-                    trailing: FlutterLogo(),
-                    title: Text('to fill the available space.'),
-                  ),
-                ),
-              ],
+            Card(
+              child: ListTile(
+                title: Text('One-line with leading widget'),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => AddNote()));
+                },
+              ),
             ),
-      ElevatedButton(
-      child: Text("Logout"),
-    onPressed: () {
-    //Sign out when user presses log out btn
-    FirebaseAuth.instance.signOut().then((value) {
-    //Go back to the main screen when logout is pressed
-    print("Signed Out");
-    Navigator.push(context,
-    MaterialPageRoute(builder: (context) => MyApp()));
-    });
-    },
-    ),
-    ],
-    ),
-    ),
-    ));
+            Card(
+              child: ListTile(
+                leading: FlutterLogo(size: 72.0),
+                title: Text('Three-line ListTile'),
+                subtitle: Text(
+                    'A sufficiently long subtitle warrants three lines.'
+                ),
+                trailing: Icon(Icons.more_vert),
+                isThreeLine: true,
+              ),
+            ),
 
+            ElevatedButton(
+              child: Text("Logout"),
+              onPressed: () {
+                //Sign out when user presses log out btn
+                FirebaseAuth.instance.signOut().then((value) {
+                  //Go back to the main screen when logout is pressed
+                  print("Signed Out");
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyApp()));
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
